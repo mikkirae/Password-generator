e7a0aec5c6d0f33d715f34c7c0cba77fa3293f98
Mikki Rae <mikkibrack@yahoo.com>
2022-03-31 23:16:06 -0400

    Working on adding event listner

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
-​
-const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-​
-const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-​
-const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function userSelected() {
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+​
+var numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+​
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+​
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+Const passwordText= document.getElementById ("password");
+Const length = document.getElementById("length");
+Const incNumbers = document.getElementById("numbers");
+Const incSymbols = document.getElementById("Symdols");
+Const generateBtn = document.getElementById("generate");
+
+
+
 
 }
 
-function randomSelected(RandomizedArr) [
-
-]
-
-Function generatePassword
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+return password:
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate buttonfunction clickHandler(event) {
+  const btn = document.querySelector('.btn');
+btn.addEventListener('click', function(event){
+   console.log('Button Clicked');
+});
+  
+generateBtn.addEventListener("click", generatePassword);
+generatePassword();
